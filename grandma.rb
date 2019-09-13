96958a1d65b04e5af2f9db9534daa7a782f94e24
John Schmidley <tlg8n@outlook.com>
2019-09-13 11:28:52 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,6 +14,8 @@
 def speak_to_grandma(phrase)
 if phrase == "I love you grandma!".upcase
   return "I love you too pumpkin!".upcase
-  
+  elsif
+  phrase == phrase.upcase
+  return "Huh?! speak up, sunny!".upcase
 end
 end
